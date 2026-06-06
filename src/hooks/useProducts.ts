abb72be55abc4c7c7c6b8fc3e28e05cc4326d5fe
luisflabio9-svg <luisflabio9@gmail.com>
2026-06-06 16:24:22 -0500

    Update useProducts.ts

--- src/hooks/useProducts.ts
+++ src/hooks/useProducts.ts
@@ -1,7 +1,7 @@
 import { useState, useEffect } from 'react';
 import { Product } from '../types';
 import { mockProducts } from '../mock-data';
-import { loadProducts, saveAllProducts } from '../firebase/products';
+import { loadProducts, saveAllProducts } from '../firebaseProducts';
 
 const STORAGE_KEY = 'electronicosjapon_products';
 
