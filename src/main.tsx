149e99c9786131ce5943d13aeb22ff4b997714e9
luisflabio9-svg <luisflabio9@gmail.com>
2026-06-11 17:04:36 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// Registrar Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker.register('/sw.js')
