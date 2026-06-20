906f7fb25e5cd727f68ecf633778e56f31e45d27
luisflabio9-svg <luisflabio9@gmail.com>
2026-06-20 15:27:03 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// Registrar Service Worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
     navigator.serviceWorker
